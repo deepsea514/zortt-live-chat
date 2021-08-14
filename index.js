aa9d311cafd9fd1c1855586ade29896d2f98b229
Stero <Stero-19@gmail.com>
2021-08-14 02:22:18 +0100

    port 3478

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const io = require("socket.io")(server, {
     origin: "*",
   },
 });
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3478;
 // const port = 3000;
 
 server.listen(port, "127.0.0.1", () => {
